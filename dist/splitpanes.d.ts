c111dfb1945ea482d29cf73e879ddd55051a9cfa
clemvnt <clemvnt@gmail.com>
2020-11-30 11:42:59 +0100

    3.0.7

---
+++ dist/splitpanes.d.ts
@@ -0,0 +1,4 @@
+import { DefineComponent } from "vue"
+
+export const Pane: DefineComponent
+export const Splitpanes: DefineComponent
--- rollup.config.js
+++ rollup.config.js
@@ -3,13 +3,19 @@ import scss from 'rollup-plugin-scss'
 
 export default {
   input: 'node_modules/splitpanes/src/components/splitpanes/index.js',
-  output: {
-    file: 'dist/splitpanes.js',
-    format: 'es'
-  }, 
+  output: [
+    {
+      file: 'dist/splitpanes.esm.js',
+      format: 'es'
+    }, 
+    {
+      file: 'dist/splitpanes.cjs.js',
+      format: 'cjs'
+    }
+  ], 
   plugins: [
     vue(),
-    scss()
+    scss({ output: 'dist/splitpanes.css' })
   ],
   external: ['vue']
 }
